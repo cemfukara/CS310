45f043add762067c6670fbdb36e34f0f7db82a4c
taha.guler <taha.guler@hotmail.com>
2025-10-13 21:07:36 +0300

    Inital commit

---
+++ bin/cs310.dart
@@ -0,0 +1,5 @@
+import 'package:cs310/cs310.dart' as cs310;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cs310.calculate()}!');
+}
---
+++ test/cs310_test.dart
@@ -0,0 +1,8 @@
+import 'package:cs310/cs310.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
